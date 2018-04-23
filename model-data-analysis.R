7b6a9024c42ad72cdf9aa726e364c35bad412d53
Josh de Leeuw <josh.deleeuw@gmail.com>
2018-04-23 11:13:08 -0400

    update model analysis

--- model-data-analysis.R
+++ model-data-analysis.R
@@ -1,18 +1,36 @@
 library(dplyr)
+library(tidyr)
 library(purrr)
 library(ggplot2)
 
 targets <- data.result %>% 
   map(function(x){
-    df <- data.frame(target=x$target, choice=x$options)
-    df <- df %>% mutate(selected=choice %in% x$result)
+    df <- data.frame(target=x$target, choice=x$options) %>% 
+      mutate(model.selected=choice %in% x$result) %>%
+      mutate(human.selected=choice %in% x$responses)
     return(df)
   }) %>%
   rbind_all()
 
-summary.data <- targets %>% group_by(target, choice) %>% summarize(p.select = sum(selected)/n())
+summary.data <- targets %>% group_by(target, choice) %>% 
+  summarize(model = sum(model.selected)/n(), human = sum(human.selected)/n())
 
-ggplot(summary.data, aes(x=choice, y=p.select))+
-  geom_bar(stat='identity')+
+summary.data <- summary.data %>% gather("source", "probability", 3:4)
+
+error.data <- summary.data %>%
+  spread("source", "probability") %>%
+  mutate(error = (human-model)^2) %>%
+  group_by(target) %>%
+  summarize(model.error = mean(error))
+
+ggplot(summary.data, aes(x=choice, y=probability, color=source))+
+  geom_point()+
   facet_wrap(~target)+
-  theme_minimal()
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+ggplot(error.data, aes(x=reorder(target,-model.error), y=model.error))+
+  geom_bar(stat='identity')+
+  labs(x="Musical Excerpt", y="Model Error")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
